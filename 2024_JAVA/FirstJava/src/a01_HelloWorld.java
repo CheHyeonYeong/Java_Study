ad907b982287844aa63c60c6f151e14e3e2eb52d
CheHyeonyeong <gusdudco6@naver.com>
2024-03-06 17:00:22 +0900

    hello world

--- 2024_JAVA/FirstJava/src/a01_HelloWorld.java
+++ 2024_JAVA/FirstJava/src/a01_HelloWorld.java
@@ -1,7 +1,30 @@
+/*
+ * new prj create : 
+ * 	file ->new -> java prj -> 프로젝트 이름 입력
+ * new class file create:
+ * 	scr-> 우클릭 -> new -> class -> 클래스 이름 지정
+ 
+ * - 자바의 특징
+    - OOP : Object Oriented Programming : 객체지향 프로그래밍
+        - 수 많은 객체들이 서로 상호작용으로 프로그래밍을 한다.
+    - 플랫폼 독립적(아키텍쳐 중립적)
+        - JVM만 다름 → 자바 사용하는 가장 큰 이유
+    - 멀티 스레드(Multi-thread) 지원 → 여러개의 쓰레드를 사용하여 동시에 작업을 수정
+    - 인터프리터 언어 - 인터프리터와 컴파일을 동시에 실행 가능
+        - 한 줄씩 읽기 및 한 파일에 모두 쓰기가 가능
+    - 많은 수의 개발자가 java를 사용 -> 라이브러리가 많아 작업이 수월하다.
+ */
+
+//ctrl + y : 되돌리기 , ctrl + / : 블록 주석처리(한 번 더는 주석 해제), ctrl + M : 화면 넓게 쓰기
+//F2 : 에러 정보 및 해결책 제공
 
 public class a01_HelloWorld {
+	
 	public static void main(String[] args) {
-		System.out.print(false);
+		int i = 3;
+		System.out.println(false);
+		System.out.printf("%d",i);
 		
 	}
 }
+
