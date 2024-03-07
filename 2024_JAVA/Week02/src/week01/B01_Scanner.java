f6ad5f3a128c93467f8587a215f7004f0aaf7328
CheHyeonyeong <gusdudco6@naver.com>
2024-03-07 16:18:07 +0900

    ✅ For test
    Scanner

---
+++ 2024_JAVA/Week02/src/week01/B01_Scanner.java
@@ -0,0 +1,13 @@
+package week01;
+import java.util.Scanner;
+
+public class B01_Scanner {
+
+	public static void main(String[] args) {
+		// Scanner 클래스로 객체를 하나 생성, 변수에 보관하여 작업..
+		Scanner s = new Scanner(System.in);
+		
+
+	}
+
+}
