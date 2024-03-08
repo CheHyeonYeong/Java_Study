e0f1458265bae60fadae4a2288e407aa33e75e34
CheHyeonyeong <gusdudco6@naver.com>
2024-03-08 15:43:26 +0900

    ✅ B_for_Quiz
    for 문 연습

---
+++ 2024_JAVA/Quiz/src/week01/B_for_Quiz.java
@@ -0,0 +1,37 @@
+package week01;
+
+public class B_for_Quiz {
+	
+	public static void one() {
+		//100부터 1까지 순서대로 출력하기
+		for(int i = 100; i>=1 ;i--) {
+			System.out.println(i);
+		}
+	}
+	public static void two() {
+			//1~200사이의 수 중 2||3의 배수가 아닌 수 들의 총 합
+		int sum=0;
+		for (int j=1; j<=200;j++) {
+			if (!(j%2==0 || j%3==0)) {
+				sum+=j;
+			}
+		}
+		System.out.println(sum);
+	}
+	public static void three() {
+		//1+(1+2)+(1+2+3) +....+(1+2+3+...+10)의 결과를 구하기
+		int sum =0;
+		for (int i=1;i<=10;i++) {
+			for (int j = 1;j<i;j++) {
+				sum +=j;
+			}
+		}
+		System.out.println(sum);
+		
+	}
+	public static void main(String[] args) {
+		one();
+//		two();
+//		three();
+	}
+}
---
+++ 2024_JAVA/Week02/src/week01/B04_ForExample.java
@@ -0,0 +1,9 @@
+package week01;
+
+public class B04_ForExample {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
