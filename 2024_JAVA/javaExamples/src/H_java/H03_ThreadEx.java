6ef448173959bda6bc1b46199d4d4ed115089228
CheHyeonyeong <gusdudco6@naver.com>
2024-03-27 13:01:20 +0900

    🚀 Study yeild
    yeild study

--- 2024_JAVA/javaExamples/src/H_java/H03_ThreadEx.java
+++ 2024_JAVA/javaExamples/src/H_java/H03_ThreadEx.java
@@ -32,6 +32,8 @@ public class H03_ThreadEx {
 
 		t1.start();
 
+
+        //------------------------------------
 		// interrupt() 메서드
 		try {
 			Thread.sleep(1000);
@@ -47,18 +49,39 @@ public class H03_ThreadEx {
 		t2.start();
 		t3.start();
 		try {
-			t2.join();   // t1 스레드가 종료될때까지 대기
-			t3.join();   // t2 스레드가 종료될때까지 대기
+			t2.join();   // t2 스레드가 종료될때까지 대기
+			t3.join();   // t3 스레드가 종료될때까지 대기
 		} catch (InterruptedException e) {
 			System.out.println(e.getMessage());
 		}
 		System.out.println("두 스레드의 Sum의 합계 : "+(t2.sum + t3.sum));
 
+		//yeild() 일드
+        YeildThread t4 = new YeildThread();
+        YeildThread t5 = new YeildThread();
+        t4.start();
+        t5.start();
 
-		//yeild() 
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+        }
+        
+        t4.iscontinue = false; //t4 스레드 yeild() 즉, 양보
+
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {        }
+
+        t4.iscontinue = true;
+        try {
+            Thread.sleep(20);
+        } catch (Exception e) {        }
+
+        t4.isbreak = true;
+        t5.isbreak = true;
 
 	}
-
 }
 
 class Sum extends Thread {
@@ -69,4 +92,23 @@ class Sum extends Thread {
 			sum += i;
 		}
 	}
+}
+
+class YeildThread extends Thread{
+    boolean isbreak = false;
+    boolean iscontinue = true;
+
+    @Override
+    public void run() {
+        while(!isbreak){
+            if(iscontinue){
+                System.out.println(getName()+" 실행중 >.<");
+            }
+            else{
+                System.out.println("양보합니다.");
+                Thread.yield(); //실행중이 아니라면 양보해라
+            }
+        }
+        System.out.println(getName()+" 종료");
+    }
 }
