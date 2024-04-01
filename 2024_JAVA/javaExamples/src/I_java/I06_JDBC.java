95a19401d677676ffc7fd57560ed0f733c3e4306
chaehyeonyeong <gusdudco6@naver.com>
2024-04-01 12:01:58 +0900

    omg

---
+++ 2024_JAVA/javaExamples/src/I_java/I06_JDBC.java
@@ -0,0 +1,20 @@
+package I_java;
+import java.sql.Connection;
+import java.sql.SQLException;
+public class I06_JDBC {
+    public static void main(String[] args) {
+        Connection conn = null;
+
+        try {
+            
+        } catch (ClassNotFoundException e) {
+            // TODO: handle exception
+        } catch(SQLException e){
+
+        }
+        finally{
+
+        }
+    }
+
+}
