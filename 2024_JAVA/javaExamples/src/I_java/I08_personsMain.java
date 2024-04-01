54aa44fe94d8f450e507a7726e1936d80aa4c726
CheHyeonYeong <gusdudco6@naver.com>
2024-04-01 17:15:34 +0900

    finally

--- 2024_JAVA/javaExamples/src/I_java/I08_personsMain.java
+++ 2024_JAVA/javaExamples/src/I_java/I08_personsMain.java
@@ -42,12 +42,44 @@ public class I08_personsMain {
         for(PersonsVO pvo : list){
             pvo.toString();
         }
+		System.out.println("특정 id를 가진 Persons 출력");
+		System.out.print("id를 입력해주세요");
+		int ids = s.nextInt();
+		PersonsVO svo = dao.selectOne(ids);
+		System.out.println(svo);
 
-        // vo = dao.selectOne(1);
-        // System.out.println(vo.toString());
+		System.out.println("<<수정하기>>");
+		System.out.print("특정 id 선택하세요 : ");
+		int id2 = s.nextInt();
+		PersonsVO uVo = dao.selectOne(id2);
+		System.out.print("수정할 성을 입력하세요("+uVo.getLastName()+") : ");
+		String lastName = s.next();
+		if(!lastName.equals("")) {
+			vo.setLastName(lastName);(lastName);
+		}
+		System.out.print("수정할 이름을 입력하세요("+uVo.getFirstName()+") : ");
+		String firstName = s.next();
+		if(!lastName.equals("")) {
+			vo.setFirstName(firstName);
+		}
+		System.out.print("수정할 나이을 입력하세요("+uVo.getAge()+") : ");
+		int age = s.nextInt();
+		if(age != 0 && age >= 0) {
+			vo.setAge(age);
+		}
+		System.out.print("수정할 도시 입력하세요("+uVo.getCity()+") : ");
+		String city = s.next();
+		if(!city.equals("")) {
+			vo.setCity(city);
+		}
 
-        vo = dao.selectOne("user");
-        System.out.println(vo.toString());
-    
+		int result = dao.update(uVo);
+		if(result != 0) {
+			System.out.println("수정 성공!!!");
+		}else {
+			System.out.println("수정 실패!!!");
+		}
+
+		s.close();
     }
 }
