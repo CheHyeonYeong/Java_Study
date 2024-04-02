598a3bbbb03bf6d16c3d036b851763148d53bb73
CheHyeonYeong <gusdudco6@naver.com>
2024-04-02 11:46:27 +0900

    omg

--- 2024_JAVA/javaExamples/src/I_java/I10_MariaDB.java
+++ 2024_JAVA/javaExamples/src/I_java/I10_MariaDB.java
@@ -16,53 +16,49 @@ public class I10_MariaDB {
 
         try { 
 
-            Class.forName(null); //드라이버가 
-            
-            //Connection 객체 생성
-            String dbFile = "SQLiteDB.db"; //실제 DB에 있는 경로
-            String url = "jdbc:sqlite:"+dbFile;
-            conn = DriverManager.getConnection(url);
+            Class.forName("org.mariadb.jdbc.Driver"); //드라이버를 로드한다.
+
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost:13306/testdb", "root", "root");
 
             System.out.println("정상 작동합니다..");
 
-            stmt = conn.createStatement();
-            String sql = "Create table if not exists testDB ("+
-                            "id interger primary key, "+
-                            "lastname text(20) not null, "+
-                            "firstname text(20), age Numeric, city text(20) default 'Seoul')";
-            int result = stmt.executeUpdate(sql);
-            System.out.println("sql 처리결과 : "+(result));
+            // stmt = conn.createStatement();
+            // String sql = "Create table if not exists testDB ("+
+            //                 "id interger primary key, "+
+            //                 "lastname text(20) not null, "+
+            //                 "firstname text(20), age Numeric, city text(20) default 'Seoul')";
+            // int result = stmt.executeUpdate(sql);
+            // System.out.println("sql 처리결과 : "+(result));
+
+            // //crud 테스트 하세요 update insert delete select
 
 
-            //crud 테스트 하세요 update insert delete select
+            // //Create 
+            // String sql1 = "insert into testDB (id, firstname, lastname, age, city)"
+            // + "values(1, 'jdbcTest','testuser13',24,'Seoul')";
 
+            // result = stmt.executeUpdate(sql1);
 
-            //Create 
-            String sql1 = "insert into testDB (id, firstname, lastname, age, city)"
-            + "values(1, 'jdbcTest','testuser13',24,'Seoul')";
-
-            result = stmt.executeUpdate(sql1);
-
-            //Read 
-            String sql2 = "select * from testDB";
-            rs = stmt.executeQuery(sql2);
-            while(rs.next()) {    
-                int id1 = rs.getInt("id");
-                String firstname = rs.getString("firstname");
-                String lastname = rs.getString("lastname");
-                int age = rs.getInt("age");
-                String city = rs.getString("city");
+            // //Read 
+            // String sql2 = "select * from testDB";
+            // rs = stmt.executeQuery(sql2);
+            // while(rs.next()) {    
+            //     int id1 = rs.getInt("id");
+            //     String firstname = rs.getString("firstname");
+            //     String lastname = rs.getString("lastname");
+            //     int age = rs.getInt("age");
+            //     String city = rs.getString("city");
                 
-                System.out.printf("ID: %d, First Name: %s, Last Name: %s, Age: %d, City: %s", id1, firstname, lastname, age, city);
-            }
+            //     System.out.printf("ID: %d, First Name: %s, Last Name: %s, Age: %d, City: %s", id1, firstname, lastname, age, city);
+            // }
 
-            //Update
-            String sql3 = "update testDB set firstname = '순신', lastname = '이'"+"where id = 1";
-            result = stmt.executeUpdate(sql3);
+            // //Update
+            // String sql3 = "update testDB set firstname = '순신', lastname = '이'"+"where id = 1";
+            // result = stmt.executeUpdate(sql3);
 
-            //Delete
-            String sql4 = "Delete from testDB where id = 1";
-            result = stmt.executeUpdate(sql3);
+            // //Delete
+            // String sql4 = "Delete from testDB where id = 1";
+            // result = stmt.executeUpdate(sql3);
         
         
 
